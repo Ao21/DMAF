3f84a71902fe4a49b42d37cc1c0d1a6ed5527ef6
unknown <ro.brett@gmail.com>
2014-08-11 09:56:17 +1000

    g

--- app/scripts/directives/menu.js
+++ app/scripts/directives/menu.js
@@ -41,7 +41,25 @@ app.directive('menu', function($compile, $timeout,$location) {
                         title: 'learn about'
                     }]
                 }, {
-                    section: 'Inductions & Access'
+                    section: 'Inductions & Access',
+                    links: [{
+                        icon:'SICU',
+                        link:'sicu',
+                        title:'SICU Safety Induction and Competency Unit'
+                    },{
+                        icon:'laserCutting',
+                        link:'laserCutting',
+                        title:'Laser Cutting Inductions'
+                    },{
+                        icon: '3dPrinting',
+                        link: '3dprinting',
+                        title: '3D printing Inductions'
+                    },
+                    {
+                        icon: 'access',
+                        link: 'access',
+                        title: 'Access'
+                    }]
                 }, {
                     section: 'Media & Events'
                 }, {
@@ -52,8 +70,43 @@ app.directive('menu', function($compile, $timeout,$location) {
                 scope.currentHover = 'pos0';
                 scope.menuHover = function(item) {
                     scope.currentHover = 'pos' + item;
+                    resizeMenu(scope.currentHover);
+                    
 
                 }
+
+                angular.element(window).bind('resize', function(){
+                    var wd = angular.element(window).width();
+                    angular.element('.menuBlock').width(wd);
+                    resizeMenu(scope.currentHover);
+                })
+
+
+                function resizeMenu(item){
+                    switch(item){
+                        case "pos0":
+                        angular.element('.menuSubContainer').css('transform','translateX(0px)');
+                        break;
+                        case "pos1":
+                        angular.element('.menuSubContainer').css('transform','translateX(-'+angular.element(window).width() * 1+'px)')
+                        break;
+                        case "pos2":
+                        angular.element('.menuSubContainer').css('transform','translateX(-'+angular.element(window).width() * 2+'px)')
+                        break;
+                        case "pos3":
+                        angular.element('.menuSubContainer').css('transform','translateX(-'+angular.element(window).width() * 3+'px)')
+                        break;
+                        case "pos4":
+                        angular.element('.menuSubContainer').css('transform','translateX(-'+angular.element(window).width() * 4+'px)')
+                        break;
+
+                    }
+                }
+
+ 
+
+                
+
 
                 scope.isActive = function (viewLocation) {
                      var active = (viewLocation === $location.path());
@@ -66,6 +119,8 @@ app.directive('menu', function($compile, $timeout,$location) {
                     var mySVGsToInject = $('.svgInject');
                     //console.log(mySVGsToInject);
                     SVGInjector(mySVGsToInject);
+                    var wd = angular.element(window).width();
+                    angular.element('.menuBlock').width(wd);
                 });
 
 
